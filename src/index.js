076bdcf14ff13940602e89000d8eebca06693a08
bhoomtawath-kreditech <bhoomtawath.plinsut@kreditech.com>
2019-11-06 18:53:02 +0700

    Support @hapi/joi: 16

--- src/index.js
+++ src/index.js
@@ -5,26 +5,15 @@ const countries = require('i18n-iso-countries')
 module.exports = joi => {
   return {
     base: joi.string(),
-    name: 'string',
-    language: {
-      IsoCountryInvalidFormat: 'needs to be a valid ISO 3166-1 alpha-2 country code',
+    type: 'country',
+    messages: {
+      'country.base': '"value" needs to be a valid ISO 3166-1 alpha-2 country code',
     },
-    pre(value, state, options) {
-      return value
+    validate(value, helpers) {
+      if (countries.isValid(value)) {
+        return { value: value.toUpperCase() }
+      }
+      return { value, errors: helpers.error('country.base') };
     },
-    rules: [
-      {
-        name: 'country',
-        setup(params) {
-          this._flags.country = true
-        },
-        validate(params, value, state, options) {
-          if (countries.isValid(value)) {
-            return value.toUpperCase()
-          }
-          return this.createError('string.IsoCountryInvalidFormat', { value }, state, options)
-        },
-      },
-    ],
   }
 }
